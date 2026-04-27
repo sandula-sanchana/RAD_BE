7b2805d8c1db1a4f1a7c56ae33efb06bb33f8265
sandula-sanchana <sandulasanchana@gmail.com>
2026-04-27 12:49:01 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,54 @@
+import express from "express";
+import mongoose, { model, Schema } from "mongoose"
+
+mongoose.connect("mongodb://localhost:27017/first_cus_db").then(() => {
+    console.log("MongoDB Connected");
+}).catch(err => {
+    console.log(err)
+})
+
+const app = express();
+
+app.use(express.json())
+//app.use(cors())
+
+interface Customer{
+    id: number
+    name: string
+    email: string
+    address?: string
+}
+
+const CustomerSchema = new Schema<Customer>({
+    id:{type:Number, required:true, unique:true},
+    name:{type:String, required:true},
+    email:{type:String, required:true},
+    address:{type:String}
+})
+
+const  customerModel=model<Customer>("Customers",CustomerSchema)
+
+app.post("/api/v1/customers/save",async (req,res)=>{
+
+    try {
+        const data = req.body
+        const cus=new customerModel({
+            id:Date.now(),
+            ...data,
+
+        })
+
+        const savedCustomer=await cus.save()
+
+        res.status(200).json({message:"success",success:savedCustomer})
+
+    }catch(err){
+        console.log(err)
+        res.status(500).json({message:"Something went wrong"})
+    }
+
+})
+
+app.listen(5000,()=>{
+    console.log(`Express server started `);
+})
